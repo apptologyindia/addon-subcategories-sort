00ffc95851e48416051d8574f5bdca30dd874963
Roman <admiralexo@gmail.com>
2016-09-06 14:14:16 +0400

    [+] {@17401} Modification of core files were moved to a separate add-on for How To: Sort Subcategories by Name on the Category Page

---
+++ app/addons/subcategories_sort/func.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!defined('BOOTSTRAP')) { die('Access denied'); }
+
+function fn_subcategories_sort_get_categories_pre(&$params, &$lang_code) {
+    $default_params = array(
+        'category_id' => 0,
+        'visible' => false,
+        'current_category_id' => 0,
+        'simple' => true,
+        'plain' => false,
+        'limit' => 0,
+        'item_ids' => '',
+        'group_by_level' => true,
+        'get_images' => false,
+        'category_delimiter' => '/',
+        'get_frontend_urls' => false,
+        'max_nesting_level' => null,
+        'sort_by' => 'name',
+        'sort_order' => 'asc',
+    );
+
+    $params = array_merge($default_params, $params);
+}
